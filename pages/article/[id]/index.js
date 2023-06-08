7cae638d79a7ab6bffbcbb7a1c3ef2228cdc7483
Samuel O. Asare <asare11samuel@gmail.com>
2023-06-08 02:44:16 +0000

    feat: dynamic pages with getStaticProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -15,9 +15,22 @@ export default function Article({article}) {
   )
 }
 
-export const getServerSideProps = async (context)=>{
+export const getStaticProps = async (context)=>{
     const res =  await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
     const article = await res.json()
 
     return {props: {article}}
+}
+
+export const getStaticPaths = async ()=>{
+    const res =  await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+
+    const ids = articles.map(article=>article.id)
+    const paths = ids.map(id=>({params: {id:id.toString()}}))
+
+    return{
+        paths,
+        fallback: false
+    }
 }
